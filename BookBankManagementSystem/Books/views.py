6be528bf8dd5931b9a5e10e04e9bcf72dea31d44
visaal1582892 <visaal1582892@gmail.com>
2024-04-02 08:53:30 +0530

    made some changes

--- BookBankManagementSystem/Books/views.py
+++ BookBankManagementSystem/Books/views.py
@@ -7,8 +7,6 @@ import datetime
 
 def addNewBooks(request):
     if request.method == "POST":
-        current_admin_user = request.user
-        current_admin_customer = Customer.objects.get(uid=current_admin_user)
         bookName = request.POST['bookName']
         author = request.POST['author']
         description = request.POST['description']
@@ -18,89 +16,63 @@ def addNewBooks(request):
         existing_books = Book.objects.filter(bname=bookName, bauthor=author)
         print(department)
         current_department = Department.objects.get(dname=department)
-        params={'current_admin_user' : current_admin_user,
-                    'current_admin_customer' : current_admin_customer,
-                    'departments' : departments,
-                    'semesters' : semesters,
-                    'customer_type' : customer_type}
         if len(existing_books) != 0:
             messages.error(request, 'Book Already Existed')
-            return render(request, 'Registration/index.html', params)
+            return redirect('index')
         new_book = Book.objects.create(bname=bookName, bauthor=author, bdescription=description, did=current_department, bsem=semester, bcount=bookCount)
         new_book.save()
         messages.success(request, 'New Book successfully Added')
-        return render(request, 'Registration/index.html', params)
+        return redirect('index')
     return HttpResponse('Nothing')
 def addExistingBooks(request):
-    current_admin_user = request.user
-    current_admin_customer = Customer.objects.get(uid=current_admin_user)
     if request.method == "POST":
         bookId = int(request.POST['bookId'])
         bookCount = int(request.POST['bookCount'])
-        params={'current_admin_user' : current_admin_user,
-                    'current_admin_customer' : current_admin_customer,
-                    'departments' : departments,
-                    'semesters' : semesters,
-                    'customer_type' : customer_type}
         try:
             current_book = Book.objects.get(pk=bookId)
         except Book.DoesNotExist:
             messages.error(request, 'Given BookId does not exist')
-            return render(request, 'Registration/index.html', params)
+            return redirect('index')
         current_book.bcount = current_book.bcount + bookCount
         current_book.save()
         messages.success(request, 'Books successfully Added')
-        return render(request, 'Registration/index.html', params)
+        return redirect('index')
     return HttpResponse('Nothing')
 def issueBook(request):
     if request.method == 'POST':
-        current_admin_user = request.user
-        current_admin_customer = Customer.objects.get(uid=current_admin_user)
         userId = request.POST['userId']
         bookId = request.POST['bookId']
-        params={'current_admin_user' : current_admin_user,
-                    'current_admin_customer' : current_admin_customer,
-                    'departments' : departments,
-                    'semesters' : semesters,
-                    'customer_type' : customer_type}
         try:
             current_student = Customer.objects.get(pk=userId)
             current_book = Book.objects.get(pk=bookId)
         except (Customer.DoesNotExist, Book.DoesNotExist):
             messages.error(request, 'Either given user id or book id does not exist')
-            return render(request, 'Registration/index.html', params)
+            return redirect('index')
         odate = datetime.date.today()
         orders = Order.objects.filter(cid=current_student, bid=current_book, odate=odate)
         if len(orders) != 0:
             messages.error(request, 'order already existed')
-            return render(request, 'Registration/index.html', params)
+            return redirect('index')
         current_book.bcount -= 1
         duedate = odate + datetime.timedelta(days=borrowDays)
         current_order = Order.objects.create(cid=current_student, bid=current_book, odate=odate, duedate=duedate)
         current_order.save()
         current_book.save()
         messages.success(request, 'Book Issued successfully')
-        return render(request, 'Registration/index.html', params)
+        return redirect('index')
     return HttpResponse('Nothing')
 
 def collectBook(request):
     if request.method == 'POST':
-        current_admin_user = request.user
-        current_admin_customer = Customer.objects.get(uid=current_admin_user)
         userId = request.POST['userId']
         bookId = request.POST['bookId']
-        params={'current_admin_user' : current_admin_user,
-                    'current_admin_customer' : current_admin_customer,
-                    'departments' : departments,
-                    'semesters' : semesters,
-                    'customer_type' : customer_type}
         try:
             current_student = Customer.objects.get(pk=userId)
             current_book = Book.objects.get(pk=bookId)
             current_order = Order.objects.get(cid=current_student, bid=bookId)
         except (Customer.DoesNotExist, Book.DoesNotExist, Order.DoesNotExist):
             messages.error(request, 'Either given user id or book id does not exist or no order registered with Given credentials')
-            return render(request, 'Registration/index.html', params)
+            return redirect('index')
         current_book.bcount += 1
         today = datetime.date.today()
         duedate = current_order.duedate
@@ -109,7 +81,7 @@ def collectBook(request):
         current_order.delete()
         current_book.save()
         messages.success(request, 'Book Collected successfully')
-        return render(request, 'Registration/index.html', params)
+        return redirect('index')
     return HttpResponse('Nothing')
 def myDues(request):
     myDues = []
